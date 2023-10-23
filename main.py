ab4f4e5b46be87a2272df17cfb4cb47f5382cf01
billo-w <35383950+billo-w@users.noreply.github.com>
2023-10-23 01:05:52 +0100

    Madlib Project
    Added a new project

---
+++ main.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+career = input("Your dream career: ")
+company_name = input("Which company would you like to work for?: ")
+
+madlib = (f"This project's difficulty is so {adj}! But, no matter the difficulty, I should proceed in to achieving the "
+          f"career I need without fail which is to become a {career} at a high prestige company such as {company_name}")
+
+print(madlib)
